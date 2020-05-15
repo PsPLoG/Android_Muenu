32564a45a5b59ec254ea3e01b4bf1c028fb41fde
PsPLoG <gkgkgkrj@gmail.com>
2020-05-15 21:38:02 +0900

    project upload

---
+++ app/src/main/java/com/psplog/muenu/ItemDetailFragment.kt
@@ -0,0 +1,47 @@
+package com.psplog.muenu
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.psplog.muenu.dummy.DummyContent
+import kotlinx.android.synthetic.main.activity_item_detail.*
+import kotlinx.android.synthetic.main.item_detail.view.*
+
+class ItemDetailFragment : Fragment() {
+    private var item: DummyContent.DummyItem? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        arguments?.let {
+            if (it.containsKey(ARG_ITEM_ID)) {
+                item = DummyContent.ITEM_MAP[it.getString(ARG_ITEM_ID)]
+                activity?.toolbar_layout?.title = item?.content
+            }
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val rootView = inflater.inflate(R.layout.item_detail, container, false)
+
+        // Show the dummy content as text in a TextView.
+        item?.let {
+            rootView.item_detail.text = it.details
+        }
+
+        return rootView
+    }
+
+    companion object {
+        /**
+         * The fragment argument representing the item ID that this fragment
+         * represents.
+         */
+        const val ARG_ITEM_ID = "item_id"
+    }
+}
