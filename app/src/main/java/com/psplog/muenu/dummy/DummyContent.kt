668ce478c83051fce448878e78134aef4b45f90e
unknown <gkgkgkrj@gmail.com>
2020-06-07 23:53:46 +0900

    add recyclerView

--- app/src/main/java/com/psplog/muenu/dummy/DummyContent.kt
+++ app/src/main/java/com/psplog/muenu/dummy/DummyContent.kt
@@ -3,28 +3,14 @@ package com.psplog.muenu.dummy
 import java.util.ArrayList
 import java.util.HashMap
 
-/**
- * Helper class for providing sample content for user interfaces created by
- * Android template wizards.
- *
- * TODO: Replace all uses of this class before publishing your app.
- */
 object DummyContent {
 
-    /**
-     * An array of sample (dummy) items.
-     */
     val ITEMS: MutableList<DummyItem> = ArrayList()
-
-    /**
-     * A map of sample (dummy) items, by ID.
-     */
     val ITEM_MAP: MutableMap<String, DummyItem> = HashMap()
 
     private const val COUNT = 25
 
     init {
-        // Add some sample items.
         for (i in 1..COUNT) {
             addItem(createDummyItem(i))
         }
@@ -42,15 +28,12 @@ object DummyContent {
     private fun makeDetails(position: Int): String {
         val builder = StringBuilder()
         builder.append("Details about Item: ").append(position)
-        for (i in 0..position - 1) {
+        for (i in 0 until position) {
             builder.append("\nMore details information here.")
         }
         return builder.toString()
     }
 
-    /**
-     * A dummy item representing a piece of content.
-     */
     data class DummyItem(val id: String, val content: String, val details: String) {
         override fun toString(): String = content
     }
